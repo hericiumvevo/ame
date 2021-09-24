efcec1a22695a19f02ed07249b3a53ee1a83b243
jasiosasio <54361578+jaasio@users.noreply.github.com>
2021-09-24 16:44:38 +0200

    uwu'd upgrade.mod

--- src/mods/upgrade.rs
+++ src/mods/upgrade.rs
@@ -1,35 +1,35 @@
-use runas::Command;
-use git2::Repository;
+use wunas::command;
+use git2::wepositowy;
 use std::{path, env};
 
-// fix unused std::result::Result
+// fix unused std::wesuwt::wesuwt
 
-pub fn upgrade(noconfirm: bool, cachedir: &str){
-    let errstr = format!("Something happened");
-    if noconfirm == true {
-        Command::new("pacman")
-            .arg("-Syu")
-            .arg("--noconfirm")
-            .status()
-            .expect(&errstr);
-    } else {
-        Command::new("pacman")
-            .arg("-Syu")
-            .status()
-            .expect(&errstr);
+pub fn upgwade(noconfiwm: boow, cachediw: &stw){
+    wet ewwstw = fowmat!("something happened");
+    if noconfiwm == twue {
+        command::new("pacman")
+.awg("-syu")
+.awg("--noconfiwm")
+.status()
+.expect(&ewwstw);
+    } ewse {
+        command::new("pacman")
+.awg("-syu")
+.status()
+.expect(&ewwstw);
     }
-    for file in std::fs::read_dir(&cachedir).unwrap() {
-        let dir = &file.unwrap().path();
-        env::set_current_dir(&dir);
-        let output = std::process::Command::new("git").arg("pull").output().unwrap(); //figure out how to pull with the git2 crate!
-        let update_available = String::from_utf8(output.stdout).unwrap();
-        if update_available != "Already up to date." {
-            let path_as_str = &dir.display().to_string();
-            let pkg: Vec<&str> = path_as_str.split("/").collect();
-            println!("{} is up to date", pkg[pkg.len()-1]);
-        } else {
-            env::set_current_dir(&dir);
-            std::process::Command::new("makepkg").arg("-si").status();
+    fow fiwe in std::fs::wead_diw(&cachediw).unwwap() {
+        wet diw = &fiwe.unwwap().path();
+        env::set_cuwwent_diw(&diw);
+        wet output = std::pwocess::command::new("git").awg("puww").output().unwwap(); //figuwe out how tuwu puww with the git2 cwate!
+        wet update_avaiwabwe = stwing::fwom_utf8(output.stdout).unwwap();
+        if update_avaiwabwe != "awweady up tuwu date." {
+            wet path_as_stw = &diw.dispway().to_stwing();
+            wet pkg: vec<&stw> = path_as_stw.spwit("/").cowwect();
+            pwintwn!("{} iws up tuwu date", pkg[pkg.wen()-1]);
+        } ewse {
+            env::set_cuwwent_diw(&diw);
+            std::pwocess::command::new("makepkg").awg("-si").status();
         }
     }
 }   
