0a982865e3063f990e4b4ac0c947b48798ebab9f
jasiosasio <54361578+jaasio@users.noreply.github.com>
2021-09-24 16:43:08 +0200

    uwu'd install mod

--- src/mods/help.rs
+++ src/mods/help.rs
@@ -1,13 +1,13 @@
-pub fn help() { // work on a proper error message, the python ame one isnt really better in my opinion
-    println!("\
-Usage:\n
-ame -S <pkg>  - install a package
-ame -f <pkg>  - install a package via flatpak
-ame -s <pkg>  - install a package via snap
-ame -R <pkg>  - remove  a package
-ame -Syu      - system upgrade
-ame -Ss <pkg> - search for a package
-ame -Sa <pkg> - search for a package over the aur
-ame -Sr <pkg> - search for a package over the repos 
-ame -Cc       - clear package cache")
+pub fn hewp() { // wowk own a pwopew ewwow message, the python ame owne isnt weawwy bettew in my opinion
+    pwintwn!("\
+usage:\n
+ame -s <pkg>  - instaww a package
+ame -f <pkg>  - instaww a package via fwatpak
+ame -s <pkg>  - instaww a package via snap
+ame -w <pkg>  - wemove  a package
+ame -syu      - system upgwade
+ame -ss <pkg> - seawch fow a package
+ame -sa <pkg> - seawch fow a package ovew the auw
+ame -sw <pkg> - seawch fow a package ovew the wepos 
+ame -cc       - cweaw package cache")
 }
